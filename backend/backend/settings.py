a70f62f88fc6b8e47aa0d272bd6caf6a54b60fe0
Riya Horo <riyahoro74@gmail.com>
2025-04-22 08:54:01 +1030

    Changed database password as test123

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'diabetes_db',
         'USER': 'myuser',
-        'PASSWORD': 'newpassword',
+        'PASSWORD': 'test123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
