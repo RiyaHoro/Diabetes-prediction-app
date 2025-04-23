22b6bd6a7da2d94cb7d147d6b101cdbc46bb9fa5
Riya Horo <riyahoro74@gmail.com>
2025-04-23 16:50:18 +1030

    Added admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import PatientData
 
-# Register your models here.
+@admin.register(PatientData)
+class PatientDataAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pregnancies', 'glucose', 'blood_pressure', 'insulin', 'bmi', 'age', 'outcome', 'DiabetesPedigreefunction')
+    search_fields = ('glucose', 'age', 'outcome')
+    list_filter = ('outcome',)
